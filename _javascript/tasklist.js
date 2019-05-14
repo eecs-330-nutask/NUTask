e07ede67d4d1b9f3de7b9e205a5091dd185c1661
Yichen <ichisaur@gmail.com>
2019-05-14 16:26:42 -0500

    basic task adding

--- _javascript/tasklist.js
+++ _javascript/tasklist.js
@@ -28,11 +28,12 @@ function addTaskOnClickEvent() {
         <strong>Add Task</strong>
         `;
     }
-
+    x.reset();
   } 
 
 function addTaskFormComplete() {
     var x = document.getElementById("add-task-form");
+    var y = document.getElementById("thisweek-section-task");
     var taskNameField = document.getElementById("task-name-field");
     var classNameField = document.getElementById("class-name-field");
     var dueDateField = document.getElementById("due-date-field");
@@ -46,7 +47,45 @@ function addTaskFormComplete() {
     //var nameValue = document.getElementById("uniqueID").value;
     //var nameValue = document.getElementById("uniqueID").value;
     //var nameValue = document.getElementById("uniqueID").value;
-console.log(taskNameValue)
+
     //x.submit();
-    x.reset();
+
+    taskObj = `
+    <div class="columns  is-vcentered task-display-hover">
+    <div class="column is-1">
+        <label class="checkbox subtitle is-5">
+          <input type="checkbox">
+        </label>
+    </div>
+
+    <div class="column is-10">
+      <div class="is-size-5" href="JavaScript:void(0)">
+        ${taskNameValue}
+      </div>
+
+      <div class="columns is-gapless is-vcentered">
+        <div class="column is-size-7 has-text-left">
+          <strong>${classNameValue}</strong>
+        </div>
+        <div class="column is-size-7 has-text-right">
+            <span class="icon">
+                <i class="fas fa-clock"></i>
+            </span>
+            ${dueDateValue}
+        </div>
+      </div>
+    </div>
+    
+    <div class="column is-1 task-display-hoverable" >
+      <a class="button is-light" href="javascript:void(0)">
+      <span class="icon">
+          <i class="fas fa-edit"></i>
+      </span>
+      </a>
+    </div>
+  </div>
+    `;
+
+    y.insertAdjacentHTML("beforeend", taskObj);
+    addTaskOnClickEvent();
  }
