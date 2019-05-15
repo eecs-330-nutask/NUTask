7b3b50f44b5d472777922e50753308ca51a8b58e
Yichen <ichisaur@gmail.com>
2019-05-14 19:05:53 -0500

    Due date fields up to date

--- _javascript/tasklist.js
+++ _javascript/tasklist.js
@@ -29,11 +29,16 @@ function addTaskOnClickEvent() {
         `;
     }
     x.reset();
+    document.getElementById("not-all-submissions").style.display = "none";
+    document.getElementById("task-add-success").style.display = "none";
   } 
 
 function addTaskFormComplete() {
     var x = document.getElementById("add-task-form");
-    var y = document.getElementById("thisweek-section-task");
+    var thisweekList = document.getElementById("thisweek-section-task");
+    var tomorrowList = document.getElementById("tomorrow-section-task");
+    var todayList = document.getElementById("today-section-task");
+
     var taskNameField = document.getElementById("task-name-field");
     var classNameField = document.getElementById("class-name-field");
     var dueDateField = document.getElementById("due-date-field");
@@ -44,12 +49,45 @@ function addTaskFormComplete() {
     var dueDateValue = dueDateField.value;
     var scheduleDateValue = scheduleDateField.value;
 
-    //var nameValue = document.getElementById("uniqueID").value;
-    //var nameValue = document.getElementById("uniqueID").value;
-    //var nameValue = document.getElementById("uniqueID").value;
 
-    //x.submit();
 
+    if (!!taskNameValue && !!classNameValue && !! dueDateValue && scheduleDateValue) {
+        console.log("nullified");
+
+
+
+        taskObjInsert(taskNameValue, classNameValue, dueDateValue,scheduleDateValue);
+
+        
+
+
+        addTaskOnClickEvent();
+        document.getElementById("task-add-success").style.display = "inline-block";
+    }
+    else {
+        document.getElementById("not-all-submissions").style.display = "inline-block";
+    }
+
+
+    
+ }
+
+ function taskObjInsert(taskNameValue, classNameValue, dueDateValue, scheduleDateValue) {
+    var thisweekList = document.getElementById("thisweek-section-task");
+    var tomorrowList = document.getElementById("tomorrow-section-task");
+    var todayList = document.getElementById("today-section-task");
+    
+    var dueDate = new Date(dueDateValue)
+    var scheduleDate = new Date(scheduleDateValue);
+    var currentDate = new Date();
+    var tomorrowDate = new Date();
+    tomorrowDate.setDate(currentDate.getDate() + 1);
+
+    var dueDateString = currentDate.toLocaleDateString(undefined, {
+        day: '2-digit',
+        month: '2-digit'
+    });
+    
     taskObj = `
     <div class="columns  is-vcentered task-display-hover">
     <div class="column is-1">
@@ -86,6 +124,13 @@ function addTaskFormComplete() {
   </div>
     `;
 
-    y.insertAdjacentHTML("beforeend", taskObj);
-    addTaskOnClickEvent();
+    if (scheduleDate.setHours(0,0,0,0) >= tomorrowDate.setHours(0,0,0,0)) {
+        thisweekList.insertAdjacentHTML("beforeend", taskObj);
+    }
+    else if (scheduleDate >= currentDate.setHours(0,0,0,0)) {
+        tomorrowList.insertAdjacentHTML("beforeend", taskObj);
+    }
+    else {
+        todayList.insertAdjacentHTML("beforeend", taskObj);
+    }
  }
