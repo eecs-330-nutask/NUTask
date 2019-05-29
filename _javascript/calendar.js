16ae89b207555eec6ff7ebe3c7ad7a32670f7eba
Yichen <ichisaur@gmail.com>
2019-05-28 22:32:08 -0500

    CSS Commit

--- _javascript/calendar.js
+++ _javascript/calendar.js
@@ -1,16 +1,31 @@
+
+var calendarEl = document.getElementById('calendar');
+
+var calendar = new FullCalendar.Calendar(calendarEl, {
+    plugins: [ 'timeGrid' ],
+    nowIndicator: true,
+    minTime: "08:00:00",
+    maxTime: "20:00:00",
+    allDaySlot: false,
+    height: $(window).height()*0.5
+});
+
+if (window.localStorage.getItem("userID") == "0") {
+    var calJSON = calList0;
+}
+if (window.localStorage.getItem("userID") == "1") {
+    var calJSON = calList1;
+}
+    
 document.addEventListener('DOMContentLoaded', function() {
-    var calendarEl = document.getElementById('calendar');
-  
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-        plugins: [ 'timeGrid' ],
-        nowIndicator: true,
-        minTime: "08:00:00",
-        maxTime: "20:00:00",
-        allDaySlot: false,
-        height: $(window).height()*0.6
-    });
+
   
     calendar.render();
+
+    for (event in calJSON) {
+
+    }
+
 
   })
   
